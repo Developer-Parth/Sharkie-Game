99dd5c94990ddd03a6ad57e0d8eea1dda1dfa8a4
Developer-Parth <166241612+Developer-Parth@users.noreply.github.com>
2025-03-09 13:53:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 import Game from './game/game.class.js';
 
 const game = new Game(document.getElementById('canvas'));
+
+document.getElementById('intro').style.opacity = "1";
+setTimeout(() => {
+    document.getElementById('intro').style.opacity = "0";
+    document.getElementById('intro').style.transition = "opacity 1s ease-out";
+    setTimeout(() => {
+        document.getElementById('intro').style.display = "none";
+    }, 1000);
+}, 1000);
